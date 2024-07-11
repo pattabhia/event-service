1fb3927ee080a3c5880d761dd8636d341fbefb9b
Pattabhi A <pattabhi.amperayani@gmail.com>
2024-07-11 12:00:00 +0530

    Test Fix

--- src/main/java/com/devcodeworld/eventservice/EventServiceApplication.java
+++ src/main/java/com/devcodeworld/eventservice/EventServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventServiceApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
         System.out.println("Hello World!");
