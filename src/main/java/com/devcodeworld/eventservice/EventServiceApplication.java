6b99347fccc8cd9e68737c290b60917499961941
Pattabhi A <pattabhi.amperayani@gmail.com>
2024-07-12 10:00:00 +0530

    adding rest endpoints

--- src/main/java/com/devcodeworld/eventservice/EventServiceApplication.java
+++ src/main/java/com/devcodeworld/eventservice/EventServiceApplication.java
@@ -8,6 +8,7 @@ public class EventServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
+        System.out.println("Event Service Application Started");
     }
 
 }
