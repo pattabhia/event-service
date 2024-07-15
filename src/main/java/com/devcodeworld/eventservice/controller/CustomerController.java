bcd39aebf965bad650cbce79f073add1c8be43e5
Pattabhi A <pattabhi.amperayani@gmail.com>
2024-07-15 10:00:00 +0530

    adding reactive specs

--- src/main/java/com/devcodeworld/eventservice/controller/CustomerController.java
+++ src/main/java/com/devcodeworld/eventservice/controller/CustomerController.java
@@ -1,4 +1,31 @@
 package com.devcodeworld.eventservice.controller;
 
+import com.devcodeworld.eventservice.dto.Customer;
+import com.devcodeworld.eventservice.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/")
+    public List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+
+    @GetMapping(value = "/reactive",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Customer> getCustomersFlux() {
+        return customerService.getCustomersByFlux();
+    }
 }
