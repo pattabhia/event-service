debacdf423beaaef0da2ffd292c22e0b53dbce88
Pattabhi A <pattabhi.amperayani@gmail.com>
2024-07-16 10:00:00 +0530

    adding reactive specs

--- src/main/java/com/devcodeworld/eventservice/dao/CustomerDao.java
+++ src/main/java/com/devcodeworld/eventservice/dao/CustomerDao.java
@@ -1,8 +1,39 @@
-package com.devcodeworld.eventservice.service;
+package com.devcodeworld.eventservice.dao;
 
+import com.devcodeworld.eventservice.dto.Customer;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@Service
-class CustomerDao {
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+@Component
+public class CustomerDao {
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Customer> getCustomers() {
+        return IntStream.range(0, 50)
+                .peek(i -> sleep(1000))
+                .peek(i -> System.out.println("processing " + i))
+                .mapToObj(i -> new Customer(i, "Customer " + i))
+                .collect(Collectors.toList());
+    }
+
+    public Flux<Customer> getCustomersByFlux() {
+        return Flux.range(0, 50)
+                .delayElements(Duration.ofSeconds(1))
+                .doOnNext(i -> System.out.println("processing " + i))
+                .map(x -> new Customer(x, "Customer " + x));
+    }
 }
