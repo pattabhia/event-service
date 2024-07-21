520adad338598dd05ba1d37a73f69422a3036350
Pattabhi A <pattabhi.amperayani@gmail.com>
2024-07-21 10:00:00 +0530

    adding reactive streams

--- src/main/java/com/devcodeworld/eventservice/entity/Product.java
+++ src/main/java/com/devcodeworld/eventservice/entity/Product.java
@@ -1,54 +1,22 @@
 package com.devcodeworld.eventservice.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "stocks")
-public class Stock {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "products")
+public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "stock_symbol", unique = true, nullable = false)
-    private String stockSymbol;
-
+    private String id;
+    private String name;
+    private int qty;
     private double price;
-
-    @Column(name = "last_updated")
-    private LocalDateTime lastUpdated;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStockSymbol() {
-        return stockSymbol;
-    }
-
-    public void setStockSymbol(String stockSymbol) {
-        this.stockSymbol = stockSymbol;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public LocalDateTime getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(LocalDateTime lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
 }
