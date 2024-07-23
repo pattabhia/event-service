ddfd989f3776129609fc6a91cb4f79a51905fb35
Pattabhi A <pattabhi.amperayani@gmail.com>
2024-07-23 10:00:00 +0530

    adding reactive streams

--- src/main/java/com/devcodeworld/eventservice/service/ProductService.java
+++ src/main/java/com/devcodeworld/eventservice/service/ProductService.java
@@ -1,41 +1,38 @@
 package com.devcodeworld.eventservice.service;
 
+import com.devcodeworld.eventservice.dto.ProductDTO;
+import com.devcodeworld.eventservice.entity.Product;
+import com.devcodeworld.eventservice.repository.ProductRepository;
+import com.devcodeworld.eventservice.utils.ProductConvertor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Range;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import com.devcodeworld.entity.Stock;
-import com.devcodeworld.grpc.StockRequest;
-import com.devcodeworld.grpc.StockResponse;
-import com.devcodeworld.grpc.StockTradingServiceGrpc;
-import com.devcodeworld.repository.StockRepository;
-import io.grpc.stub.StreamObserver;
-import org.springframework.grpc.server.service.GrpcService;
+@Service
+public class ProductService {
 
-@GrpcService
-public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
+    @Autowired
+    private ProductRepository productRepository;
 
-
-    private final StockRepository stockRepository;
-
-    public StockTradingServiceImpl(StockRepository stockRepository) {
-        this.stockRepository = stockRepository;
+    public Flux<ProductDTO> getAllProducts() {
+        return productRepository.findAll().map(ProductConvertor::toProductDTO);
     }
 
-    @Override
-    public void getStockPrice(StockRequest request,
-                              StreamObserver<StockResponse> responseObserver) {
-
-        //stockName -> DB -> map response -> return
-
-        String stockSymbol = request.getStockSymbol();
-        Stock stockEntity = stockRepository.findByStockSymbol(stockSymbol);
-
-        StockResponse stockResponse = StockResponse.newBuilder()
-                .setStockSymbol(stockEntity.getStockSymbol())
-                .setPrice(stockEntity.getPrice())
-                .setTimestamp(stockEntity.getLastUpdated().toString())
-                .build();
-
-        responseObserver.onNext(stockResponse);
-        responseObserver.onCompleted();
-
+    public Mono<ProductDTO> getProductById(String id) {
+        return productRepository.findById(id).map(ProductConvertor::toProductDTO);
     }
+
+    public Flux<ProductDTO> getProductInRange(double start, double end) {
+        return productRepository.findByPriceBetween(Range.closed(start, end));
+    }
+
+    public Mono<ProductDTO> createProduct(Mono<ProductDTO> productDTOMono) {
+        return productDTOMono.map(ProductConvertor::toProduct)
+                .flatMap(productRepository::save)
+                .map(ProductConvertor::toProductDTO);
+    }
+
 }
