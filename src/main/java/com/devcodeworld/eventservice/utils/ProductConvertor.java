a39f48bea31d0b362cd89d9bc0edaee8e520ca49
Pattabhi A <pattabhi.amperayani@gmail.com>
2024-07-24 10:00:00 +0530

    adding reactive streams

--- src/main/java/com/devcodeworld/eventservice/utils/ProductConvertor.java
+++ src/main/java/com/devcodeworld/eventservice/utils/ProductConvertor.java
@@ -1,4 +1,20 @@
 package com.devcodeworld.eventservice.utils;
 
+import com.devcodeworld.eventservice.dto.ProductDTO;
+import com.devcodeworld.eventservice.entity.Product;
+import org.springframework.beans.BeanUtils;
+
 public class ProductConvertor {
+
+    public static ProductDTO toProductDTO(Product product) {
+        ProductDTO productDTO = new ProductDTO();
+        BeanUtils.copyProperties(product, productDTO);
+        return productDTO;
+    }
+
+    public static Product toProduct(ProductDTO productDTO) {
+        Product product = new Product();
+        BeanUtils.copyProperties(productDTO, product);
+        return product;
+    }
 }
